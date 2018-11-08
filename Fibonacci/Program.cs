223bca94b626d068136f1f8e3be499f184c712f5
Shashank Srivatsa Kuchibhotla <sk.kuchibhotla@gmail.com>
2018-11-07 21:19:54 -0500

    Palindrome and Fibonacci

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,7 +6,31 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+
+            // Declaring the first two elements of the sequence
+            int first = 0;
+            int second = 1;
+            Console.WriteLine("Enter number of iterations you need: ");
+            int iteration = Convert.ToInt32(Console.ReadLine());
+
+            // Printing the first two elements of the Fibonacci Sequence
+            Console.WriteLine("The Fibonacci Sequence is: ");
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+
+            int sum = 0;
+            // Iteration
+            for(int counter = 0; counter < iteration; counter++)
+            {
+                sum = first + second;
+                Console.WriteLine(sum);
+
+                first = second;
+                second = sum;
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Palindrome/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter the number to check if it is a Palindrome or not: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            string inputInString = input.ToString();
+            int inputLength = inputInString.Length;
+
+            int copyOfInput = input;
+
+            int output = 0;
+            for(int i = inputLength; i > 0; i--)
+            {
+                int digit = copyOfInput % 10;
+                // output = output + (digit * (10 ^ (i-1)));
+
+                double power = Convert.ToDouble(i - 1);
+                // int tensMultiple = 10 ^ power;
+                double tensMultiple = Math.Pow(10.0, power);
+                double value = digit * tensMultiple;
+                int valueInInt = Convert.ToInt32(value);
+                output = output + valueInInt;
+
+                copyOfInput = copyOfInput / 10;
+            }
+
+            Console.WriteLine("The reverse of " + input + " is: " + output);
+
+            int difference = output - input;
+            if(difference != 0)
+            {
+                Console.WriteLine("The value entered is not a palindrome. ");
+            }
+            else
+            {
+                Console.WriteLine("The value entered is a palindrome. ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
